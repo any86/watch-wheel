c7f94a335a3f9a5731e309639aab25a4ae2ae61f
any86 <383514580@qq.com>
2022-01-01 16:32:22 +0800

    refactor: vx/vy改成velocityX/velocityY

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # any-wheel
 🖱️ 监控鼠标滚动
 
-开发中...
+## 安装
+```shell
+yarn add any-wheel
+```
+
+## 使用
+
+```javascript
+import watchWheel from 'any-wheel';
+// 任意元素
+const el = document.getElementById('any');
+
+// 监视鼠标指针在该元素上触发的滚轮滚动.
+watchWheel(el, e=>{
+    console.log(e.type) // wheelstart / wheelmove / wheelend
+    console.log(e.delatY) // 鼠标滚轮滚动一下的距离
+    console.log(e.velocityY) // 滚动速度, 
+});
+```
+如果你的鼠标滚轮**支持x轴滚动**, 那么你还可以监听到`delatX`和`velocityX`的数值变化.
+
+
+
--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 // https://developer.mozilla.org/zh-CN/docs/Web/API/Element/wheel_event
 const { setTimeout } = window;
-const WHEEL = 'wheel';
+const TYPE_WHEEL = 'wheel';
+const TYPE_WHEEL_START = `${TYPE_WHEEL}start` as const;
+const TYPE_WHEEL_MOVE = `${TYPE_WHEEL}move` as const;
+const TYPE_WHEEL_END = `${TYPE_WHEEL}end` as const;
+type TYPE_WHEEL_NAME = typeof TYPE_WHEEL_START | typeof TYPE_WHEEL_MOVE | typeof TYPE_WHEEL_END;
 
 /**
  * 统一滚轮的表现
@@ -10,7 +14,7 @@ const WHEEL = 'wheel';
  * @param PAGE_HEIGHT 滚动一页的高度
  * @returns 一次滚动的偏移
  */
-function normalizeWheel(e: WheelEvent, LINE_HEIGHT = 40, PAGE_HEIGHT = 800) {
+function normalizeWheel(e: WheelEvent, LINE_HEIGHT = 40, PAGE_HEIGHT = 800):[number,number] {
     let { deltaX, deltaY, deltaMode } = e;
     // console.log(deltaMode, e.DOM_DELTA_LINE);
     if (deltaMode == e.DOM_DELTA_LINE) {
@@ -22,70 +26,108 @@ function normalizeWheel(e: WheelEvent, LINE_HEIGHT = 40, PAGE_HEIGHT = 800) {
     }
     return [deltaX, deltaY];
 }
+
 interface WheelEvent2 {
-    target:EventTarget|null;
-    type: 'start' | 'move' | 'end';
-    [k: string]: any;
+    target: EventTarget | null;
+    type: TYPE_WHEEL_NAME;
+    velocityX: number;
+    velocityY: number;
+    deltaX: number;
+    deltaY: number;
+    nativeEvent: WheelEvent;
 }
-export default function (el: HTMLElement, onChange: (e: WheelEvent2) => void) {
+/**
+ * 增强版鼠标滚动事件监听器
+ * @param el
+ * @param onChange
+ * @returns 卸载监听器
+ */
+export default function (el: HTMLElement, onChange: (e: WheelEvent2) => void, { interval } = { interval: 166 }) {
     // 上一次滚动发生时间
     let _lastWheelTime: number | undefined;
     // wheelend延迟触发的id
     let _endTimeoutId: number;
-    // 一轮滚动距离的和
-    let _deltaYCounter = 0;
-    let _deltaXCounter = 0;
+    // 一次计算周期的滑动位移
+    let _displacementX = 0;
+    let _displacementY = 0;
+    // 速度
+    let velocityX = 0;
+    let velocityY = 0;
+
+    const refreshVelocity = intervalCounter(timeDiff => {
+        velocityX = _displacementX / timeDiff;
+        velocityY = _displacementY / timeDiff;
+        _displacementX = 0;
+        _displacementY = 0;
+        console.log({ velocityY });
+    }, 16);
 
     function __onWheel(e: WheelEvent) {
         const [deltaX, deltaY] = normalizeWheel(e);
-        _deltaXCounter += deltaX;
-        _deltaYCounter += deltaY;
+        _displacementX += deltaX;
+        _displacementY += deltaY;
+
         /**
          * 触发回调和自定义DOM事件
-         * @param e 滚轮事件
          * @param type 类型: 'start' | 'move' | 'end'
-         * @param payload 自定义事件数据
          */
-        function _dispatchEvent(type: 'start' | 'move' | 'end', payload?: Record<string, any>) {
+        function _dispatchEvent(type: TYPE_WHEEL_NAME) {
             const wheelEvent2 = {
-                target:e.target,
+                target: e.target,
                 deltaX,
                 deltaY,
                 type,
-                ...payload,
+                velocityX,
+                velocityY,
+                nativeEvent: e,
             };
 
-            const event = new Event('wheel' + type);
+            const event = new Event(TYPE_WHEEL + type);
             onChange(wheelEvent2);
             el.dispatchEvent(event);
         }
 
+        // 刷新速度
+        refreshVelocity();
+
         // 最后一下滚动
         clearTimeout(_endTimeoutId);
         _endTimeoutId = setTimeout(() => {
-            const timeDiff = Date.now() - <number>_lastWheelTime;
-            const vx = _deltaXCounter / timeDiff;
-            const vy = _deltaYCounter / timeDiff;
-
-            _lastWheelTime = void 0;
-            _deltaXCounter = 0;
-            _deltaYCounter = 0;
-            _dispatchEvent('end', { vx, vy });
-        }, 16);
+            _dispatchEvent(TYPE_WHEEL_END);
+        }, interval);
 
         // 开始
         if (void 0 === _lastWheelTime) {
-            _dispatchEvent('start');
+            velocityX = 0;
+            velocityY = 0;
+            _dispatchEvent(TYPE_WHEEL_START);
         }
         // 移动
         else {
-            _dispatchEvent('move');
+            _dispatchEvent(TYPE_WHEEL_MOVE);
         }
         _lastWheelTime = Date.now();
     }
 
-    el.addEventListener(WHEEL, __onWheel);
+    el.addEventListener(TYPE_WHEEL, __onWheel);
     return () => {
-        el.removeEventListener(WHEEL, __onWheel);
+        el.removeEventListener(TYPE_WHEEL, __onWheel);
     };
+}
+
+function intervalCounter(callback: (timeDiff: number) => void, interval: number) {
+    // 记录触发时间
+    let lastTime: number | undefined;
+    return () => {
+        const now = Date.now();
+        if (void 0 === lastTime) {
+            lastTime = now;
+        } else {
+            const timeDiff = now - lastTime;
+            if (timeDiff > interval) {
+                lastTime = undefined;
+                callback(timeDiff);
+            }
+        }
+    }
 }
